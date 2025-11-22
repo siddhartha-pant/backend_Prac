c3713382a4c872ae2c92877afd549643d73d4276
siddhartha-pant <iamsiddhartha23@gmail.com>
2025-11-22 11:48:20 +0530

    revised app file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/notes", noteRoutes);
+
+module.exports = app;
