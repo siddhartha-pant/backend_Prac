356a319bae1a30e9bc0b6eeaaf6d6d3575a1886f
siddhartha-pant <iamsiddhartha23@gmail.com>
2025-12-10 12:41:20 +0530

    changes made in the server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const app = require("./app");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+dotenv.config();
+
+const app = require("./app"); // <-- IMPORTANT
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
   })
   .catch((err) => console.error("MongoDB connection error:", err));
